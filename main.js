3a435ea4a99e295b020d0676c4a4c556c39c6990
c0113248 <c0113248fd@edu.teu.ac.jp>
2017-01-15 15:18:44 +0900

    2017/1/15 15:18:44 の時点の状態

--- main.js
+++ main.js
@@ -11,13 +11,31 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 let editWindows;
+const fs = require('fs');
+const exec = require('child_process').exec;
 
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
-    app.quit();
+    LastCommit();
   }
 });
+function LastCommit(){
+  var _dir = fs.readFileSync(__dirname+"\\"+"last_dir.txt");
+  exec('git add --all ' + _dir + "\\", function (error, stdout, stderr) {
+          if (error == null) {
+            LastCommit2();
+          }
+      });
+}
+function LastCommit2(){
+  var d = new Date();
+  exec('git commit -m "' + d.toLocaleString() + ' の時点の状態"', function (error, stdout, stderr) {
+    if(error == null){
+          app.quit();      
+    }
+  });
+}
 
 // Electronの初期化完了後に実行
 app.on('ready', function() {
